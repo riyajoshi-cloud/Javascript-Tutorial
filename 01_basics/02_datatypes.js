dab1d5bf37b8ccf9052a91a3e37ccf15b69f2c54
Riya Joshi <joshiriya467@gmail.com>
2026-03-22 14:41:53 +0000

    day3 02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+// New js- it has changed because of new objects, features etc
+//1. New code new treatment
+
+"use strict"; // treat all JS code as newer version : it happens default though but it's good to write it.
+
+// alert(3 + 3) // 2. error: we are using nodejs not browser
+
+//3.READABILITY AND SPACING
+console.log(3+3)   // can't write both console at same line without semicolons.
+console.log("Riya") 
+
+// Use proper spacing that increses readability.
+
+//4. DATATYPES
+
+// number => 2^53 and bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value: representation of empty value
+// undefined=> declaration but not value
+// symbol: uniqueness in components, used in react basically, Figma
+
+
+//object
+
+let name ="Riya"  //String
+let age = 20      // integer
+let isLoggedIn = true  // boolean
+
+console.log(typeof "riya")  // type of any var tell
+console.log(typeof null);  //o/p : object :which is a loopehole in js
+console.log(typeof undefined); // undefined
